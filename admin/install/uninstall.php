991966aaae722836b1ffde3064c94cf0bd7df083
Matias Griese <matias@kunena.org>
2012-02-22 12:10:00 +0200

    Create Joomla! 2.5 installer with proper language support

--- admin/install/uninstall.php
+++ admin/install/uninstall.php
@@ -2,12 +2,14 @@
 /**
  * Podcast Professional - The Joomla Podcast Manager
  * @package 	Podcast Professional
- * @copyright 	(C) 2011 Kontent Design. All rights reserved.
+ * @copyright 	(C) 2011-2012 Kontent Design. All rights reserved.
  * @copyright 	(c) 2005-2008 Joseph L. LeBlanc
  * @license 	http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link 		http://extensions.kontentdesign.com
  **/
 defined('_JEXEC') or die();
+
+// WARNING: this file is in 2 locations!
 
 function com_uninstall ()
 {
